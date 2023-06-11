46ffcb7cd4f2db6913c917ee168f367daceb63a2
Michael <michaelmees99@gmail.com>
2023-06-11 05:28:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -71,6 +71,8 @@ app.delete('/api/notes/:id', (req, res) => {
     });
   });
 
-app.listen(3000, () => {
-  console.log('Server is listening on PORT 3000');
-});
+  const port = process.env.PORT || 3000; // FINALLY!! this is what lets me use heroku's dynamic port
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
